76426dda807c52ca4d5c04ae6a89540dbd0c96e5
Serhii Naumenko <s.naumenko.miy@gmail.com>
2022-06-24 22:05:33 +0300

    add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,3 +13,26 @@ module.exports = {
   rules: {
   },
 };
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
+};
